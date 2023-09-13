4e1c014582a98f9dc9f22fac2a9c8a8561528bd8
linhobs <gyamenahyawpius@gmail.com>
2023-09-13 14:53:31 +0200

    fix: complete fixes for swe-task-1

--- client_test.py
+++ client_test.py
@@ -7,15 +7,20 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
-    """ ------------ Add the assertion below ------------ """
 
+    """ ------------ Add the assertion below ------------ """
+    expected_result = ('ABC', 120.48, 121.2, 120.84)
+    actual_result = getDataPoint(quotes[0])
+    self.assertEqual(expected_result, actual_result)
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    expected_result = ('ABC', 120.48, 119.2, 119.84)
+    actual_result = getDataPoint(quotes[0])
+    self.assertEqual(expected_result, actual_result)
 
   """ ------------ Add more unit tests ------------ """
 
